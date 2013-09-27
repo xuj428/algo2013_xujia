7c61d6fc7af9972c05e9aa93a620c90cc443ad33
xuj428 <xuj428@newschool.edu>
2013-09-27 01:22:14 -0400

    make it move, add some color

--- week 3/recursivePractice/src/rectangle.cpp
+++ week 3/recursivePractice/src/rectangle.cpp
@@ -3,21 +3,41 @@
 #include "rectangle.h"
 
 circle::circle(){
+    red=100;
+    green=60;
+    blue=150;
+    
     
 }
 
-void circle::drawCircle(float x, float y,float r){
+void circle::drawCircle(float x, float y, float r){
     ofCircle(x, y,r);
+   ofSetColor(red,green,blue);
     
-    ofRotate(30);
-    if(r>2){
+    ofRotate(angle);
+    
+    if(r>4){
         
        
        drawCircle(x+r/2,y,r/2);
         drawCircle(x,y+r/2,r/2);
         
     }
+}
 
-    
-    
+
+void circle::update(){
+    angle=angle+0.01;
+    red +=0.2;
+    green +=0.5;
+    blue +=0.5;
+    if (red>255) {
+        red=0;
+    }
+    if (blue>255){
+        blue=0;
+    }
+    if (green>255) {
+        green=0;
+    }
 }
--- week 3/recursivePractice/src/rectangle.h
+++ week 3/recursivePractice/src/rectangle.h
@@ -5,5 +5,9 @@ class circle{
 public:
     circle();
     void drawCircle(float x, float y, float r);
+    void update();
     ofPoint pos;
+    float radius;
+    float angle;
+    float red,green,blue;
 };
